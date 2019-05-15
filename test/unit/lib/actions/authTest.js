4691dbb39fad2762127ce823c74707bb2a353b49
Phil Rukin <philipp@rukin.me>
2019-05-15 17:56:48 +0200

    Add `applicationKeyId` parameter
    to fix #66 and #64

--- test/unit/lib/actions/authTest.js
+++ test/unit/lib/actions/authTest.js
@@ -31,69 +31,115 @@ describe('actions/auth', function() {
     describe('authorize', function() {
         var b2;
 
-        beforeEach(function() {
-            b2 = {
-                accountId: 'unicorns',
-                applicationKey: 'rainbows'
-            };
-        });
-
-        describe('with valid response', function() {
-            beforeEach(function(done) {
-
-                authResponse = {
-                    data: {
-                        authorizationToken: 'foo',
-                        apiUrl: 'https://foo',
-                        downloadUrl: 'https://bar'
-                    }
+        describe('using accountId', function() {
+            beforeEach(function() {
+                b2 = {
+                    accountId: 'unicorns',
+                    applicationKey: 'rainbows'
                 };
+            });
 
-                // Resolve the promise that's set up above.
-                deferred.resolve(authResponse);
+            describe('with valid response', function() {
+                beforeEach(function(done) {
 
-                auth.authorize(b2).then(function(response) {
-                    actualAuthResponse = response;
-                    done();
+                    authResponse = {
+                        data: {
+                            accountId: 'unicorns',
+                            authorizationToken: 'foo',
+                            apiUrl: 'https://foo',
+                            downloadUrl: 'https://bar'
+                        }
+                    };
+
+                    // Resolve the promise that's set up above.
+                    deferred.resolve(authResponse);
+
+                    auth.authorize(b2).then(function(response) {
+                        actualAuthResponse = response;
+                        done();
+                    });
+                });
+
+                it('Should set correct auth header in request options', function() {
+                    expect(actualAuthResponse).to.eql(authResponse);
+                    expect(requestOptions.headers).to.eql({ Authorization: 'Basic dW5pY29ybnM6cmFpbmJvd3M=' });
+                });
+
+                it('Should set auth request options and set fields on valid b2 instance', function() {
+                    expect(b2.accountId).to.be('unicorns');
+                    expect(b2.authorizationToken).to.be('foo');
+                    expect(b2.apiUrl).to.be('https://foo');
+                    expect(b2.downloadUrl).to.be('https://bar');
                 });
             });
 
-            it('Should set correct auth header in request options', function() {
-                expect(actualAuthResponse).to.eql(authResponse);
-                expect(requestOptions.headers).to.eql({ Authorization: 'Basic dW5pY29ybnM6cmFpbmJvd3M=' });
-            });
+            describe('with error response', function() {
+                var isRejected;
+                var rejectedMessage;
 
-            it('Should set auth request options and set fields on valid b2 instance', function() {
-                expect(b2.authorizationToken).to.be('foo');
-                expect(b2.apiUrl).to.be('https://foo');
-                expect(b2.downloadUrl).to.be('https://bar');
+                beforeEach(function(done) {
+                    errorMessage = 'Something went wrong';
+                    isRejected = false;
+                    rejectedMessage = null;
+
+                    // Reject the promise that's set up above
+                    deferred.reject(errorMessage);
+
+                    auth.authorize(b2).then(null, function(error) {
+                        isRejected = true;
+                        rejectedMessage = error;
+                        done();
+                    });
+                });
+
+                it('Should reject promise is error is received', function() {
+                    expect(isRejected).to.be(true);
+                    expect(rejectedMessage).to.be(errorMessage);
+                });
             });
         });
 
-        describe('with error response', function() {
-            var isRejected;
-            var rejectedMessage;
-
-            beforeEach(function(done) {
-                errorMessage = 'Something went wrong';
-                isRejected = false;
-                rejectedMessage = null;
-
-                // Reject the promise that's set up above
-                deferred.reject(errorMessage);
-
-                auth.authorize(b2).then(null, function(error) {
-                    isRejected = true;
-                    rejectedMessage = error;
-                    done();
-                });
+        describe('using applicationId', function() {
+            beforeEach(function() {
+                b2 = {
+                    applicationKeyId: 'kittens',
+                    applicationKey: 'rainbows'
+                };
             });
 
-            it('Should reject promise is error is received', function() {
-                expect(isRejected).to.be(true);
-                expect(rejectedMessage).to.be(errorMessage);
+            describe('with valid response', function() {
+                beforeEach(function(done) {
+
+                    authResponse = {
+                        data: {
+                            accountId: 'unicorns',
+                            authorizationToken: 'foo',
+                            apiUrl: 'https://foo',
+                            downloadUrl: 'https://bar'
+                        }
+                    };
+
+                    // Resolve the promise that's set up above.
+                    deferred.resolve(authResponse);
+
+                    auth.authorize(b2).then(function(response) {
+                        actualAuthResponse = response;
+                        done();
+                    });
+                });
+
+                it('Should set correct auth header in request options', function() {
+                    expect(actualAuthResponse).to.eql(authResponse);
+                    expect(requestOptions.headers).to.eql({ Authorization: 'Basic a2l0dGVuczpyYWluYm93cw==' });
+                });
+
+                it('Should set auth request options and set fields on valid b2 instance', function() {
+                    expect(b2.accountId).to.be('unicorns');
+                    expect(b2.authorizationToken).to.be('foo');
+                    expect(b2.apiUrl).to.be('https://foo');
+                    expect(b2.downloadUrl).to.be('https://bar');
+                });
             });
         });
     });
-
 });
